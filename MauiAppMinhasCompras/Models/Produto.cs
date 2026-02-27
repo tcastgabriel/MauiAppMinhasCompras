e728e6a0bc0308d099814496cb7946e54e92f732
TcasT <TcasT@DESKTOP-FCQ5TRL>
2026-02-26 22:08:43 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppMinhasCompras/Models/Produto.cs
@@ -0,0 +1,15 @@
+﻿using SQLite;
+
+namespace MauiAppMinhasCompras.Models
+{
+    public class Produto
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        
+        public double Quantidade { get; set; }
+
+        public double Preco {  get; set; }
+    }
+}
